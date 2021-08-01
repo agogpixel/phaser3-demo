395a0bfbf25807d40ac50772484f47dda3f1dc18
kidthales <kidthales@agogpixel.com>
2021-08-01 07:15:34 +0000

    Further tinkerings.

--- src/app/scenes/main-scene.ts
+++ src/app/scenes/main-scene.ts
@@ -7,6 +7,9 @@ export class MainScene extends Phaser.Scene {
    */
   public static readonly key = 'Main';
 
+  /**
+   * Keyboard controls.
+   */
   private controls: Phaser.Cameras.Controls.FixedKeyControl;
 
   /**
@@ -20,6 +23,7 @@ export class MainScene extends Phaser.Scene {
    * Lifecycle method called before all others.
    */
   public init(): void {
+    // Initially disable input.
     this.input.keyboard.enabled = false;
   }
 
@@ -27,46 +31,86 @@ export class MainScene extends Phaser.Scene {
    * Lifecycle method called after init & preload.
    */
   public create(): void {
+    // Scene fade in.
     this.cameras.main.setAlpha(0);
-
     this.events.once(Phaser.Scenes.Events.TRANSITION_START, (fromScene, duration) => {
       this.tweens.add({
         targets: this.cameras.main,
         alpha: 1,
         duration,
         delay: 1000,
+        // Allow input after fade in complete.
         onComplete: () => (this.input.keyboard.enabled = true)
       });
     });
 
-    const map = this.make.tilemap({ key: 'mapA' });
-    const tileset = map.addTilesetImage('roguelikeCity_transparent');
-    const layer1 = map.createLayer('Tile Layer 1', tileset).setScale(4);
-    const layer2 = map.createLayer('Tile Layer 2', tileset).setScale(4);
+    const scaleFactor = 4;
 
+    // Background.
+    const tilemap = this.make.tilemap({ key: 'mapA', insertNull: true });
+    const tileset = tilemap.addTilesetImage('roguelikeCity_transparent');
+    const layer = tilemap.createLayer('Tile Layer 1', tileset).setScale(scaleFactor);
+    tilemap.createLayer('Tile Layer 2', tileset).setScale(scaleFactor);
+
+    // Lightposts.
+    const cx = (tilemap.tileWidth / 2) * scaleFactor;
+    const cy = (tilemap.tileHeight / 2) * scaleFactor;
+    const v = new Phaser.Math.Vector2();
+    layer.tileToWorldXY(7, 9, v);
+    const lp1 = this.add.image(v.x + cx, v.y + cy + (cy / 2), 'lightpost', 'right').setScale(scaleFactor).setOrigin(0.2, 1).setDepth(9);
+    layer.tileToWorldXY(23, 9, v);
+    const lp2 = this.add.image(v.x + cx, v.y + cy + (cy / 2), 'lightpost', 'right').setScale(scaleFactor).setOrigin(0.2, 1).setDepth(9);
+    layer.tileToWorldXY(15, 15, v);
+    const lp3 = this.add.image(v.x + cx, v.y + cy - (cy / 2), 'lightpost', 'left').setScale(scaleFactor).setOrigin(0.8, 1).setDepth(15);
+
+    // Rain particle emitter.
     const rain = this.add.particles('blue').createEmitter({
-      x: { min: 0, max: layer1.displayWidth },
+      x: { min: 0, max: layer.displayWidth },
       y: 0,
       lifespan: 1000,
       speedY: 1500,
       scaleY: { min: 1, max: 4 },
       scaleX: .01,
       quantity: 1,
-      blendMode: 'LIGHTEN',
+      blendMode: 'SCREEN',
     });
 
-    const cursors = this.input.keyboard.createCursorKeys();
+    // Lighting.
+    const shadow = this.add.graphics();
+    shadow.depth = 1000;
+    shadow.fillStyle(0x000000, 0.75);
+    shadow.fillRect(0, 0, layer.displayWidth, layer.displayHeight);
 
+    // TODO: Lighting.
+    /*const t1 = this.make.graphics({});
+    t1.fillStyle(0xffffff, 1);
+    t1.beginPath();
+    t1.moveTo(585, 825);
+    t1.lineTo(435, 1000);
+    t1.lineTo(735, 1000);
+    t1.fill();
+    const mask = t1.createGeometryMask();
+    mask.invertAlpha = true;
+    shadow.setMask(mask);*/
+
+    // Keyboard controls.
+    const cursors = this.input.keyboard.createCursorKeys();
     this.controls = new Phaser.Cameras.Controls.FixedKeyControl({
       camera: this.cameras.main,
       left: cursors.left,
       right: cursors.right,
       up: cursors.up,
       down: cursors.down,
-      speed: 0.5
+      speed: 0.5,
+      // TODO: weird
+      zoomIn: this.input.keyboard.addKey('MINUS'),
+      zoomOut: this.input.keyboard.addKey('PLUS'),
+      maxZoom: 3,
+      minZoom: 0.5
     });
 
-    this.cameras.main.setBounds(0, 0, layer1.displayWidth, layer1.displayHeight);
+    // Set camera bounds to tilemap's current display dimensions.
+    this.cameras.main.setBounds(0, 0, layer.displayWidth, layer.displayHeight);
   }
 
   public update(time: number, delta: number): void {
--- src/app/scenes/preload-scene.ts
+++ src/app/scenes/preload-scene.ts
@@ -20,9 +20,13 @@ export class PreloadScene extends Phaser.Scene {
    * Lifecycle method called after init & before create.
    */
   public preload(): void {
+    // Tileset.
     this.load.image('roguelikeCity_transparent', 'assets/roguelikeCity_transparent.png');
+
+    // Tilemap data.
     this.load.tilemapTiledJSON('mapA', 'assets/mapA.json');
 
+    // Rain particle.
     this.load.image('blue', 'assets/blue.png');
   }
 
@@ -30,6 +34,8 @@ export class PreloadScene extends Phaser.Scene {
    * Lifecycle method called after init & preload.
    */
   public create(): void {
+    this.createLightpostTexture();
+
     this.scene.transition({
       target: MainScene.key,
       duration: 1500,
@@ -46,5 +52,33 @@ export class PreloadScene extends Phaser.Scene {
         }
       }
     });
+  }
+
+  /**
+   * Create lightpost texture sheet from tileset.
+   */
+  private createLightpostTexture(): void {
+    const tilemap = this.make.tilemap({ tileWidth: 16, tileHeight: 16, width: 2, height: 3 });
+    const tileset = tilemap.addTilesetImage('roguelikeCity_transparent', 'roguelikeCity_transparent', 16, 16, 0, 1);
+
+    const layer = tilemap.createBlankLayer('layer', tileset);
+    this.children.remove(layer);
+
+    const renderTexture = this.make.renderTexture({ x: 0, y: 0, width: layer.width * 2, height: layer.height }, false);
+
+    // Left facing.
+    layer.putTileAt(593, 0, 0);
+    layer.putTileAt(630, 0, 1);
+    layer.putTileAt(667, 0, 2);
+
+    // Right facing.
+    layer.putTileAt(594, 1, 0);
+    layer.putTileAt(631, 1, 1);
+    layer.putTileAt(668, 1, 2);
+
+    const texture = renderTexture.draw(layer).saveTexture('lightpost');
+
+    texture.add('left', 0, 0, 0, 16, 48)
+    texture.add('right', 0, 16, 0, 16, 48)
   }
 }
